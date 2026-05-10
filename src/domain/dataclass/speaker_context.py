c454ea488f9d626935a77c2d6363f0365d589353
alexandre.assad <alexandre.assad.pro@gmail.com>
2026-05-10 12:58:03 +0200

    Revert ":wip: code before colab in neural parsing"
    This reverts commit ce15cd6593805b081053d952087a5a15f7c265ae.

--- src/domain/dataclass/speaker_context.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from typing import Literal
-
-@dataclass(frozen=True)
-class SpeakerContext:
-    speaker_id: str
-    l1_status: Literal["L1", "L2"]
-    gender: Literal["F", "M"]     
